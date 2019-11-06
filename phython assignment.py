11a83ef23a1ca53f832527a58641f7c4ecdbe32a
Qasim Khan <56044350+Qasimkhan786@users.noreply.github.com>
2019-11-06 13:27:15 +0500

    Add files via upload

---
+++ phython assignment.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+#"""
+##qs 01
+#print('Twinkle, twinkle, little star')
+#print('      How I Wonder what you are!')
+#print('               Up above the world so high,')
+#print('                  Like a daimond in th sky.')
+#print('Twinkle, twinkle, little star')
+#print('      How I Wonder what you are!')
+
+#
+##qs 02
+#import sys
+#print('phython version is')
+#print(sys.version)
+#print('version info.')
+#print(sys.version_info)
+#
+
+
+##qs 03
+#
+#import datetime 
+#dt = datetime.datetime.now()
+#print('todays date and time is' , dt)
+#
+
+
+##qs 04
+#
+#x=int(input('Enter radius of circle'))
+#q=x*x
+#a=(3.14*q)
+#print('Area of circle is' , a)
+
+#
+##qs 05
+#
+#q = str(input('enter the name'))
+#a=q[::-1]
+#print(a)
+#
+#qs 06
+#
+#x=int(input('enter first value'))
+#y=int(input('enter second value'))
+#print(x+y)
